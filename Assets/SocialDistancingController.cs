b3dc493bf95008b641db9a3afff549e3152a949c
Adam Chernick <32597382+acher249@users.noreply.github.com>
2020-05-02 09:55:09 -0700

    added project
    added project

---
+++ Assets/SocialDistancingController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialDistancingController : MonoBehaviour
+{
+    public Material red;
+    public Material green;
+
+    public AudioClip Siren;
+    public AudioClip Beep;
+
+    private AudioSource audioData;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "collider")
+        {
+            Debug.Log("OnCollisionEnter");
+            Debug.Log("Hit: " + collision.transform.name);
+            gameObject.GetComponent<MeshRenderer>().material = red;
+
+            audioData = GetComponent<AudioSource>();
+            audioData.clip = Siren;
+            audioData.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "collider")
+        {
+            Debug.Log("OnCollisionExit");
+            Debug.Log("No longer in contact with: " + other.transform.name);
+            gameObject.GetComponent<MeshRenderer>().material = green;
+
+            audioData = GetComponent<AudioSource>();
+            audioData.clip = Beep;
+            audioData.Play();
+        }
+    }
+}
